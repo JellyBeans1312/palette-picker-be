002526a14969627b726cd3dd600501b24ea54767
Aidan McKay <aidanmckay2000@gmail.com>
2019-10-08 13:42:02 -0600

    set up seed file and mock data

---
+++ projectData.js
@@ -0,0 +1,25 @@
+module.exports = [
+  {
+    project_name: 'project 1', 
+    palettes: [
+      {palette_name: 'palette 1', color_one: '#000', color_two: '#fff', color_three: '#333', color_four: '#FA8072', color_five: '#00FF00'}
+    ]
+  },
+  {
+    project_name: 'project 2', 
+    palettes: [
+      {palette_name: 'palette 2', color_one: '#000', color_two: '#fff', color_three: '#333', color_four: '#FA8072', color_five: '#00FF00'},
+      {palette_name: 'palette 3', color_one: '#000', color_two: '#fff', color_three: '#333', color_four: '#00FFFF', color_five: '#008000'},
+      {palette_name: 'palette 4', color_one: '#800080', color_two: '#FF00FF', color_three: '#C0C0C0', color_four: '#000080', color_five: '#008080'}
+
+    ]
+  },
+  {
+    project_name: 'project 3', 
+    palettes: [
+      {palette_name: 'palette 5', color_one: '#FFFF00', color_two: '#800000', color_three: '#808000', color_four: '#00FFFF', color_five: '#008000'},
+      {palette_name: 'palette 6', color_one: '#FF0000', color_two: '#999999', color_three: '#454545', color_four: '#F08080', color_five: '#CD5C5C'}
+
+    ]
+  }
+]
